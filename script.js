2560fa308b8e7a385e1ae8b4de108cc246e044d2
dyskio <130579182+dyskio@users.noreply.github.com>
2023-09-19 23:30:22 +0200

    fix the color and clear button functionality.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Returns a random Hex Color
 function getRandomHexColor() {
     // Generate a random integer between 0 and 16777215 (hexadecimal FFFFFF)
     const randomColor = Math.floor(Math.random() * 16777216);
@@ -9,6 +10,7 @@ function getRandomHexColor() {
     return `#${hexColor}`;
 };
 
+// Generates the required nr of etch-a-sketch squares
 const flexContainer = document.querySelector(".flex-container");
 for (let i=0; i < 256; i++){
     // generates the individual squares that will
@@ -22,13 +24,48 @@ for (let i=0; i < 256; i++){
 }
 
 const squares = document.querySelectorAll(".one-box");
-squares.forEach(function (sq) {
-    sq.addEventListener('mouseover', function (event) {
-        sq.style.backgroundColor = getRandomHexColor();
+
+// Handles the COLOR toggle being clicked
+const colorCheckbox = document.querySelector("#checkbox_color");
+colorCheckbox.addEventListener("change", function(event) {
+    // toggle on = color
+    if (event.target.checked){
+        squares.forEach(function (sq) {
+            sq.addEventListener('mouseover', function (event) {
+                sq.style.backgroundColor = getRandomHexColor();
+            });
+        });
+    } 
+    // toggle off = grey
+    else {
+        squares.forEach(function (sq) {
+            sq.addEventListener('mouseover', function (event) {
+                sq.style.backgroundColor = "grey";
+            });
+        });
+    }
+});
+
+// Handles the CLEAR toggle being clicked
+const clearCheckbox = document.querySelector("#checkbox_clear");
+clearCheckbox.addEventListener("change", function (event) {
+    // toggle on = clear screen and revert button
+    if (event.target.checked){
+        for (let i=0; i < squares.length; i++){
+            squares[i].style.backgroundColor = "#E3E4DB";
+        }
+        setTimeout(() => { clearCheckbox.checked = false;; }, 250);
+    }
+});
+
+// Activates the etch-a-sketch game on page load
+function activateEtchASketch() {
+    squares.forEach(function (sq) {
+        sq.addEventListener('mouseover', function (event) {
+            sq.style.backgroundColor = "grey";
+        });
     });
-});
+}
 
-const checkBox = document.querySelector(".checkbox");
-checkBox.addEventListener("click", function(event) {
-
-});
+// Call the function to activate the game when the page loads
+activateEtchASketch();
